37beff4a49ccd9be450fbdbc7340599d7e77eed5
Richard Martinsen <github@nav.no>
2019-08-09 09:23:19 +0200

    Lagt til beregning av maksdato

--- src/main/kotlin/no/nav/infotrygdmaksdato/controller/MaksdatoController.kt
+++ src/main/kotlin/no/nav/infotrygdmaksdato/controller/MaksdatoController.kt
@@ -1,5 +1,11 @@
 package no.nav.infotrygdmaksdato.controller
 
+import no.nav.helse.Grunnlagsdata
+import no.nav.helse.MaksdatoResult
+import no.nav.helse.Tidsperiode
+import no.nav.helse.maksdato
+import no.nav.helse.Yrkesstatus.*
+import java.time.LocalDate
 import no.nav.infotrygdmaksdato.service.PeriodeService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,14 +19,28 @@ class MaksdatoController (val periodeService: PeriodeService){
     internal var logger = LoggerFactory.getLogger(StatusController::class.java)
 
     @RequestMapping(method = [RequestMethod.GET], value = ["/v1/hentMaksdato"])
-    fun hentMaksdato(@RequestParam(name = "fnr", required = true) fnr : String) : String {
-        this.logger.info("henter maksdato")
-        this.logger.info(fnr)
+    fun hentMaksdato(@RequestParam(name = "fnr", required = true) fnr : String) : MaksdatoResult {
+        //Hente perioder fra Oracle...
+        var tidsperiode = Tidsperiode(LocalDate.parse("2019-03-01"), LocalDate.parse("2019-03-30"))
+        var perioder = mutableListOf<Tidsperiode>()
+        perioder.add(tidsperiode)
+        //
 
-        var liste = periodeService.hentPerioder(fnr)
 
-        this.logger.info(liste.toString())
+        // Finne Arbkat
+        //IS10-ARBKAT
+        //ARBEIDSTAKER = 01,
+        //SELVSTENDIG_NÆRINGSDRIVENDE = 02
+        //FRILANSER = 19
+        //IKKE_I_ARBEID = 06
 
-        return "Ok"
+
+        //Finne alder fra fnr
+
+
+        var grunnlagsdata = Grunnlagsdata(LocalDate.now(), LocalDate.now().plusDays(14), 40, ARBEIDSTAKER, perioder)
+        var maksdato = maksdato(grunnlagsdata)
+
+        return maksdato
     }
 }
